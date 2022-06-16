4f804d97852779675a27fa6979a68717639a534f
Elias Boukamza <elias.boukamza@contentsquare-ext.com>
2022-06-16 17:12:49 +0200

    feat(chrome-extension): collects adapt event metrics

--- apps/chrome-extension/src-extension/rollup.config.js
+++ apps/chrome-extension/src-extension/rollup.config.js
@@ -3,12 +3,14 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import path from 'path'
+import replace from '@rollup/plugin-replace'
 
+require('dotenv').config()
 export default [
   {
     input: path.join(__dirname, './background.ts'),
     output: [{ file: './dist/scripts/background.js', format: 'iife' }],
-    plugins: [ts(), resolve(), commonjs(), terser({ compress: true })]
+    plugins: [replace({ __MATOMO_URL: process.env.MATOMO_URL }), ts(), resolve(), commonjs(), terser({ compress: true })]
   },
   {
     input: path.join(__dirname, './readapt.ts'),
