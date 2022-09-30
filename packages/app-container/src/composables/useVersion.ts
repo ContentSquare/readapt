be48d58f60773d313b857e1a40f16b22ff5c71c9
Ahmed Radjdi <ahmed.radjdi@contentsquare.com>
2022-09-30 13:09:32 +0200

    v1.4.2 (#57)

--- packages/app-container/src/composables/useVersion.ts
+++ packages/app-container/src/composables/useVersion.ts
@@ -2,7 +2,7 @@ import { computed } from '@vue/composition-api'
 
 export function useVersion() {
   const version = computed(() => {
-    return 'v1.4.0'
+    return 'v1.4.2'
   })
   return { version }
 }
