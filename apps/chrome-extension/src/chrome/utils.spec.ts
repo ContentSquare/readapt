cb7820efa1d3d76f6718abcfe307a1c27d70edf0
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-11-16 17:03:22 +0100

    test(chrome-extension): unit test the store settings accessor from storage

--- apps/chrome-extension/src/chrome/utils.spec.ts
+++ apps/chrome-extension/src/chrome/utils.spec.ts
@@ -1,11 +1,36 @@
 import { getStoredSettings } from './utils'
 import { settings } from '../../tests/fixtures/settings'
+import { chrome } from 'jest-chrome'
 
 describe('chrome utils', () => {
   describe('getStoredSettings()', () => {
-    describe('when storage contains settings', () => {
-      it('should return the settings', () => {
-        
+    describe('when extension storage contains settings', () => {
+      it('should return the settings', async () => {
+        chrome.storage.local.get.mockImplementationOnce((key) => {
+          const storageState: Record<string, unknown> = { settings, otherKey: 'other value' }
+          if (typeof key === 'string') {
+            return {
+              [key]: storageState[key]
+            }
+          }
+        })
+
+        expect(await getStoredSettings()).toEqual(settings)
+      })
+    })
+
+    describe('when extension storage does not contain settings', () => {
+      it('should return undefined', async () => {
+        chrome.storage.local.get.mockImplementationOnce(function (key) {
+          const storageState: Record<string, unknown> = { otherKey: 'other value' }
+          if (typeof key === 'string') {
+            return {
+              [key]: storageState[key]
+            }
+          }
+        })
+
+        expect(await getStoredSettings()).toBeUndefined()
       })
     })
   })
