ef6422cb33f5085fb59f7009c98a57257cc8791d
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-11-23 12:35:09 +0100

    feature(chrome-extension): text adaptation profile entity

---
+++ apps/chrome-extension/src/entities/textAdaptationProfile/ui/TextAdaptationProfilesDropdown.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PropType } from 'vue/types/v3-component-props'
+
+const props = defineProps({
+  options: {
+    type: Array as PropType<string[]>,
+    required: true
+  }
+})
+</script>
+<template>
+  <select data-test-id="dropdown">
+    <option v-for="(option, index) in props.options" :key="index">{{ option }}</option>
+  </select>
+</template>
