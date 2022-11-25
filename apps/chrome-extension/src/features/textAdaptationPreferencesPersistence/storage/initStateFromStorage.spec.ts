697e58d6012cc8609f31dd04a3e21a91278cb325
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-11-25 15:35:13 +0100

    test(chrome-extension): add test case for empty storage

--- apps/chrome-extension/src/features/textAdaptationPreferencesPersistence/storage/initStateFromStorage.spec.ts
+++ apps/chrome-extension/src/features/textAdaptationPreferencesPersistence/storage/initStateFromStorage.spec.ts
@@ -12,16 +12,29 @@ describe('initStateFromStorage()', () => {
     useTextAdaptationPreferences().reset()
   })
 
-  it('should preferences state from storage', async () => {
-    const preferences: TextAdaptationPreferences = {
-      activeProfileId: textAdaptationProfileFixture.id,
-      profiles: [textAdaptationProfileFixture]
-    }
-    const storage = new MemoryStorage({ [STORAGE_KEY_TEXT_ADAPTATION_PREFERENCES]: preferences })
+  describe('when storage contains preferences', () => {
+    it('should initialize preferences state from storage', async () => {
+      const preferences: TextAdaptationPreferences = {
+        activeProfileId: textAdaptationProfileFixture.id,
+        profiles: [textAdaptationProfileFixture]
+      }
+      const storage = new MemoryStorage({ [STORAGE_KEY_TEXT_ADAPTATION_PREFERENCES]: preferences })
 
-    await initStateFromStorage(storage)
+      await initStateFromStorage(storage)
 
-    const { profiles } = useTextAdaptationPreferences()
-    expect(profiles.value).toEqual(preferences.profiles)
+      const { profiles } = useTextAdaptationPreferences()
+      expect(profiles.value).toEqual(preferences.profiles)
+    })
+  })
+
+  describe('when storage does not contain preferences', () => {
+    it('should default to initial state', async () => {
+      const storage = new MemoryStorage()
+
+      await initStateFromStorage(storage)
+
+      const { profiles } = useTextAdaptationPreferences()
+      expect(profiles.value).toEqual([])
+    })
   })
 })
