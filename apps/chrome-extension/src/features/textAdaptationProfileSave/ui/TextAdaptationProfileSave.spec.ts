57660fe3f914b2f09554278cb4426988256e90f9
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-11-28 19:37:05 +0100

    feat(chrome-extension): save profile - WIP

--- apps/chrome-extension/src/features/textAdaptationProfileSave/ui/TextAdaptationProfileSave.spec.ts
+++ apps/chrome-extension/src/features/textAdaptationProfileSave/ui/TextAdaptationProfileSave.spec.ts
@@ -2,9 +2,21 @@ import { mount } from '@vue/test-utils'
 import TextAdaptationProfileSave from './TextAdaptationProfileSave.vue'
 
 describe('TextAdaptationProfileSave', () => {
-  it('should render a button', () => {
-    const wrapper = mount(TextAdaptationProfileSave)
+  describe('when save button is clicked', () => {
+    describe('when the edited profile is new', () => {
+      it('should create a new profile', async () => {
+        const wrapper = mount(TextAdaptationProfileSave)
 
-    expect(wrapper.find('[data-test-id=button]').exists()).toBe(true)
+        await wrapper.find('[data-test-id=save]').trigger('click')
+
+        expect(wrapper.emitted('save')).toBeTruthy()
+      })
+    })
   })
+
+  // it('should render a button', () => {
+  //   const wrapper = mount(TextAdaptationProfileSave)
+
+  //   expect(wrapper.find('[data-test-id=button]').exists()).toBe(true)
+  // })
 })
