f5f3320ec87f6abbba142bf677b275790342d74d
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-11-30 19:18:06 +0100

    feat(chrome-extension): active profile dropdown

--- apps/chrome-extension/src/features/textProfilesActiveDropdown/ui/TextProfilesActiveDropdown.spec.ts
+++ apps/chrome-extension/src/features/textProfilesActiveDropdown/ui/TextProfilesActiveDropdown.spec.ts
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
 import TextProfilesActiveDropdown from './TextProfilesActiveDropdown.vue'
 import { textProfileFixture as profile, useTextPreferences } from '@/entities/textPreferences'
 import { TextProfileId } from '@/entities/textPreferences/model/TextPreferences'
@@ -12,20 +13,46 @@ describe('TextProfilesActiveDropdown', () => {
     useTextPreferences().reset()
   })
 
-  const factory = (profileId: TextProfileId | null = null) => {
-    const wrapper = mount(TextProfilesActiveDropdown, {
-      propsData: {
-        // value: profileId
-      }
-    })
+  const factory = () => {
+    const wrapper = mount(TextProfilesActiveDropdown)
     const dropdown = wrapper.find<HTMLSelectElement>('[data-test-id=dropdown]')
 
     return { wrapper, dropdown }
   }
 
-  it('should render profiles as options', () => {
-    const { dropdown } = factory()
+  describe('profiles dropdown', () => {
+    it('should render profiles as options', () => {
+      const { dropdown } = factory()
 
-    expect(dropdown.find(`[value="${profile.id}"]`).text()).toBe(profile.name)
+      expect(dropdown.find(`[value="${profile.id}"]`).text()).toBe(profile.name)
+    })
+
+    it('should render no active profile option', () => {
+      const { dropdown } = factory()
+
+      expect(dropdown.find('option[value=""]').text()).toBe('Disable (no active profile)')
+    })
+  })
+
+  describe('active profile', () => {
+    describe('when there is an active profile', () => {
+      it('should select the option of active profile', async () => {
+        const { dropdown } = factory()
+        const { setActiveProfileId } = useTextPreferences()
+
+        setActiveProfileId(profile.id)
+        await nextTick()
+
+        expect(dropdown.element.value).toBe(profile.id.toString())
+      })
+    })
+
+    describe('when there is no active profile', () => {
+      it('should select the option with no active profile', () => {
+        const { dropdown } = factory()
+
+        expect(dropdown.element.value).toBe('')
+      })
+    })
   })
 })
