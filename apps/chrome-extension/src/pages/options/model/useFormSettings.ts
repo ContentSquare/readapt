23109bd195b6e6956442827b7d458b0f4ad6e71a
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-03 17:58:19 +0100

    fix(chrome-extension): fix language selection and form settings in options

--- apps/chrome-extension/src/pages/options/model/useFormSettings.ts
+++ apps/chrome-extension/src/pages/options/model/useFormSettings.ts
@@ -4,16 +4,15 @@ import { buildDefaultProfiles } from '@readapt/settings'
 import cloneDeep from 'lodash/cloneDeep'
 
 export function useFormSettings(language: Ref<Language>, baseSettings: Ref<Settings | null> = ref(null)) {
-  const settingsByLanguage = ref(buildDefaultProfiles())
+  const settingsByLanguage = ref()
 
   const settings = computed(() => settingsByLanguage.value[language.value])
-  const reset = () => (settingsByLanguage.value = buildDefaultProfiles())
   const updateSettings = <K extends keyof Settings>(key: K, value: Settings[K]) => {
     settings.value[key] = value
   }
 
   watchEffect(() => {
-    reset()
+    settingsByLanguage.value = buildDefaultProfiles()
     if (baseSettings.value) {
       const { language } = baseSettings.value
       settingsByLanguage.value[language] = cloneDeep(baseSettings.value)
