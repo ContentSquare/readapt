0612668bd4489f7901442b7665fda69b06027c0e
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-03 17:42:31 +0100

    fix(chrome-extension): fix language selection and form settings in options

---
+++ apps/chrome-extension/src/features/textSettingsFileDownload/ui/TextSettingsFileDownload.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { TextSettings } from '@/entities/textPreferences'
+
+const props = defineProps<{ settings: TextSettings }>()
+
+// TODO: implement lazy generation of the JSON
+const settingsFile = computed(() => {
+  const settingsFile = encodeURIComponent(JSON.stringify(props.settings, null, 2))
+  return `data:application/json;charset=utf-8,${settingsFile}`
+})
+</script>
+<template>
+  <a :href="settingsFile" data-test-id="anchor" download="settings.json" target="_blank">{{ $t('SETTINGS.DOWNLOAD_SETTINGS') }}</a>
+</template>
