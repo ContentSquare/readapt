f7499c12534648245c42f81486d73756dfe09488
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-05 12:20:25 +0100

    feat(chrome-extension): warning

--- apps/chrome-extension/src/features/textProfileEditDropdown/ui/TextProfileEditDropdown.spec.ts
+++ apps/chrome-extension/src/features/textProfileEditDropdown/ui/TextProfileEditDropdown.spec.ts
@@ -1,18 +1,25 @@
 import TextProfileEditDropdown from './TextProfileEditDropdown.vue'
 import { mount } from '@vue/test-utils'
 import { textProfileFixture as profile, useTextPreferences } from '@/entities/textPreferences'
-import { TextProfileId } from '@/entities/textPreferences/model/TextPreferences'
+import { TextProfileId, TextSettings } from '@/entities/textPreferences/model/TextPreferences'
+import {  } from 'module';
 
 describe('TextProfileEditDropdown', () => {
   afterEach(() => {
     useTextPreferences().reset()
   })
 
-  const factory = (profileId: TextProfileId | null = null) => {
+  interface FactoryProps {
+    profileId: TextProfileId | null
+    settings: TextSettings
+  }
+
+  const factory = ({ profileId, settings }: FactoryProps) => {
     useTextPreferences().setProfiles([profile])
     const wrapper = mount(TextProfileEditDropdown, {
       propsData: {
-        value: profileId
+        value: profileId,
+        settings
       }
     })
     const dropdown = wrapper.find<HTMLSelectElement>('[data-test-id=dropdown]')
@@ -22,13 +29,13 @@ describe('TextProfileEditDropdown', () => {
 
   describe('profiles dropdown', () => {
     it('should render profiles as options', () => {
-      const { dropdown } = factory()
+      const { dropdown } = factory({ profileId: null, settings: profile.settings })
 
       expect(dropdown.find(`[value="${profile.id}"]`).text()).toBe(profile.name)
     })
 
     it('should render new profile as option', () => {
-      const { dropdown } = factory()
+      const { dropdown } = factory({ profileId: null, settings: profile.settings })
 
       expect(dropdown.find('option[value=""]').text()).toBe('New Profile')
     })
@@ -36,14 +43,14 @@ describe('TextProfileEditDropdown', () => {
 
   describe('v-model binding', () => {
     it('should select a profile using "value" as profile id', () => {
-      const { dropdown } = factory(profile.id)
+      const { dropdown } = factory({ profileId: profile.id, settings: profile.settings })
 
       expect(dropdown.element.value).toBe(profile.id.toString())
     })
 
-    describe('when a profile is selected', () => {
+    describe('when an existing profile option is selected', () => {
       it('should trigger "input" with selected profile id', () => {
-        const { wrapper, dropdown } = factory()
+        const { wrapper, dropdown } = factory({ profileId: null, settings: profile.settings })
 
         dropdown.setValue(profile.id)
 
@@ -53,11 +60,27 @@ describe('TextProfileEditDropdown', () => {
 
     describe('when a new profile option is selected', () => {
       it('should trigger "input" with undefined', () => {
-        const { wrapper, dropdown } = factory(profile.id)
+        const { wrapper, dropdown } = factory({ profileId: profile.id, settings: profile.settings })
 
         dropdown.setValue('')
 
-        expect(wrapper.emitted('input')).toEqual([[undefined]])
+        expect(wrapper.emitted('input')).toEqual([[null]])
+      })
+    })
+
+    describe('when changing selection with unsaved changes', () => {
+      it('should open a confirmation dialog', () => {
+        const { wrapper, dropdown } = factory({ profileId: profile.id, settings: profile.settings })
+
+        wrapper.setProps({
+          settings: {
+            ...profile.settings,
+            fontFamily: 'OpenDyslexic'
+          }
+        })
+        dropdown.setValue(null)
+
+
       })
     })
   })
