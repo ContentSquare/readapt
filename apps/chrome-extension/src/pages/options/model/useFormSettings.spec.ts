6734f2c62b7fb8082a61a535a6cd23f4ce00ab49
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-05 08:14:55 +0100

    test(chrome-extension): improve option page form settings model test

--- apps/chrome-extension/src/pages/options/model/useFormSettings.spec.ts
+++ apps/chrome-extension/src/pages/options/model/useFormSettings.spec.ts
@@ -1,13 +1,19 @@
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import { buildDefaultProfiles } from '@readapt/settings'
 import { useFormSettings } from './useFormSettings'
-import { textProfileFixture as profile, TextProfileId, useTextPreferences } from '@/entities/textPreferences'
+import { TextProfile, textProfileFixture as profile, TextProfileId, useTextPreferences } from '@/entities/textPreferences'
 
 describe('useFormSettings()', () => {
   const emptyProfileId = ref<TextProfileId | null>(null)
 
+  const frenchProfile: TextProfile = {
+    id: 2,
+    name: 'French profile',
+    settings: { ...buildDefaultProfiles()['fr'], fontSize: '200%' }
+  }
+
   beforeEach(() => {
-    useTextPreferences().setProfiles([profile])
+    useTextPreferences().setProfiles([profile, frenchProfile])
   })
 
   afterEach(() => {
@@ -37,7 +43,7 @@ describe('useFormSettings()', () => {
       expect(language.value).toEqual('fr')
     })
 
-    it('should change pick settings of the changed language', () => {
+    it('should pick settings of the changed language', () => {
       const { settings, setLanguage } = useFormSettings(emptyProfileId)
 
       setLanguage('fr')
@@ -48,17 +54,39 @@ describe('useFormSettings()', () => {
 
   describe('when having selected profile id', () => {
     it('should provide settings from the selected profile', () => {
-      const selectedProfileId = ref<TextProfileId>(profile.id)
+      const selectedProfileId = ref<TextProfileId>(frenchProfile.id)
       const { settings } = useFormSettings(selectedProfileId)
 
-      expect(settings.value).toEqual(profile.settings)
+      expect(settings.value).toEqual(frenchProfile.settings)
     })
 
     it('should provide language from the selected profile settings', () => {
-      const selectedProfileId = ref<TextProfileId>(profile.id)
+      const selectedProfileId = ref<TextProfileId>(frenchProfile.id)
       const { language } = useFormSettings(selectedProfileId)
 
-      expect(language.value).toEqual(profile.settings.language)
+      expect(language.value).toEqual(frenchProfile.settings.language)
+    })
+
+    describe('when selected profile id changes to null', () => {
+      it('should provide the default settings', async () => {
+        const selectedProfileId = ref<TextProfileId | null>(frenchProfile.id)
+        const { settings } = useFormSettings(selectedProfileId)
+
+        selectedProfileId.value = null
+        await nextTick()
+
+        expect(settings.value).toEqual(buildDefaultProfiles()['en'])
+      })
+
+      it('should reset the language to english', async () => {
+        const selectedProfileId = ref<TextProfileId | null>(frenchProfile.id)
+        const { language } = useFormSettings(selectedProfileId)
+
+        selectedProfileId.value = null
+        await nextTick()
+
+        expect(language.value).toBe('en')
+      })
     })
   })
 
