83e9b9f4c5a4f34c0a45bd0a849b37966f212334
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-06 14:50:32 +0100

    feat(chrome-extension): delete profile

---
+++ apps/chrome-extension/src/features/textProfileDeleteButton/ui/TextProfileDeleteButton.spec.ts
@@ -0,0 +1,90 @@
+import { mount } from '@vue/test-utils'
+import TextProfileDeleteButton from './TextProfileDeleteButton.vue'
+import { textProfileFixture as profile, useTextPreferences, TextProfileId } from '@/entities/textPreferences'
+import { mockAlert, mockConfirm } from '@/shared/test'
+
+describe('TextProfileDeleteButton', () => {
+  beforeEach(() => {
+    useTextPreferences().setProfiles([profile])
+  })
+
+  afterEach(() => {
+    useTextPreferences().reset()
+    jest.restoreAllMocks()
+  })
+
+  interface FactoryProps {
+    value: TextProfileId | null
+    confirmValue?: boolean
+  }
+
+  const factory = ({ value, confirmValue = true }: FactoryProps) => {
+    mockAlert()
+    mockConfirm(confirmValue)
+    const wrapper = mount(TextProfileDeleteButton, {
+      propsData: { value }
+    })
+
+    const triggerDelete = async () => await wrapper.find('[data-test-id=delete]').trigger('click')
+
+    return { wrapper, triggerDelete }
+  }
+
+  it.each([
+    { value: null, renders: false },
+    { value: profile.id, renders: true }
+  ])('should render or not delete button', ({ value, renders }) => {
+    const { wrapper } = factory({ value })
+
+    expect(wrapper.find('[data-test-id=delete]').exists()).toBe(renders)
+  })
+
+  describe('when clicking the button', () => {
+    it('should show a confirm', async () => {
+      const { triggerDelete } = factory({ value: profile.id })
+
+      await triggerDelete()
+
+      expect(confirm).toHaveBeenCalledWith('Are you sure you want to delete this profile? This is irreversible.')
+    })
+
+    describe('when user confirms the profile deletion', () => {
+      it('should delete the profile', async () => {
+        const { triggerDelete } = factory({ value: profile.id })
+        const { preferences } = useTextPreferences()
+
+        await triggerDelete()
+
+        expect(preferences.profiles).toHaveLength(0)
+      })
+
+      it('should show a notification about profile deletion', async () => {
+        const { triggerDelete } = factory({ value: profile.id })
+
+        await triggerDelete()
+
+        expect(alert).toHaveBeenCalledWith('The profile has been deleted')
+      })
+
+      it('should emit "input" with null', async () => {
+        const { wrapper, triggerDelete } = factory({ value: profile.id })
+        useTextPreferences().setProfiles([])
+
+        await triggerDelete()
+
+        expect(wrapper.emitted('input')).toEqual([[null]])
+      })
+    })
+
+    describe('when user cancels the profile deletion', () => {
+      it('should not delete the profile', async () => {
+        const { triggerDelete } = factory({ value: profile.id, confirmValue: false })
+        const { preferences } = useTextPreferences()
+
+        await triggerDelete()
+
+        expect(preferences.profiles).toHaveLength(1)
+      })
+    })
+  })
+})
