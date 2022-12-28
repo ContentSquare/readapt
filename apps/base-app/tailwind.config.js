c5c6466833eec0a5e9d3732e8f569c59f4caca95
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-28 16:29:07 +0100

    feat(base-app): add design system spacings to tailwind config

--- apps/base-app/tailwind.config.js
+++ apps/base-app/tailwind.config.js
@@ -166,6 +166,27 @@ module.exports = {
       bold: '600',
       'extra-bold': '700'
     },
+    spacing: {
+      '1px': '1px',
+      0.25: '2px',
+      0.5: '4px',
+      0.75: '6px',
+      1: '8px',
+      0.83: '10px',
+      1.5: '12px',
+      2: '16px',
+      3: '24px',
+      4: '32px',
+      4.5: '36px',
+      6: '48px',
+      7: '56px',
+      8: '64px',
+      9: '72px',
+      10: '80px',
+      11: '88px',
+      12: '96px',
+      15: '120px'
+    },
     extend: {
       textColor: {
         // colors text
