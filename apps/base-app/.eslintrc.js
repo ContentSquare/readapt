33cfbb5149b707fa6ea6585d34496e3cde2fcb9a
Dmitri Pavlutin <dpavlutin@gmail.com>
2022-12-29 14:29:02 +0100

    chore(base-app): add tailwind eslint plugin

--- apps/base-app/.eslintrc.js
+++ apps/base-app/.eslintrc.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier'],
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:tailwindcss/recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier'
+  ],
   rules: {
     'prettier/prettier': [
       'error',
