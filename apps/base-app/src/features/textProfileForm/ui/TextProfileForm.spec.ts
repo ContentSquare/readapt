0f277f382dd6bd130ad9dd0972cd4919ce3e26b4
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-01-09 12:37:49 +0100

    refactor(base-app): extract profile form into a separate module

--- apps/base-app/src/features/textProfileForm/ui/TextProfileForm.spec.ts
+++ apps/base-app/src/features/textProfileForm/ui/TextProfileForm.spec.ts
@@ -1,16 +1,134 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount, type Wrapper } from '@vue/test-utils'
 import TextProfileForm from './TextProfileForm.vue'
 import { textSettingsFixture as settings } from '@/entities/textPreferences'
+import type { ColoredItem } from '@readapt/settings'
 
 describe('TextProfileForm', () => {
-  it('should render a div', () => {
-    const wrapper = mount(TextProfileForm, {
+  const factory = () => {
+    const wrapper = shallowMount(TextProfileForm, {
       propsData: {
-        activeIndex: 0,
         settings
       }
     })
+    const tabs = wrapper.find('[data-test-id=tabs]')
 
-    expect(wrapper.find('div').exists()).toBe(true)
+    return { wrapper, tabs }
+  }
+
+  describe('tabs', () => {
+    const expectTabRendered = (wrapper: Wrapper<TextProfileForm>, expectedTabTestId: string) => {
+      ;['general', 'phoneme', 'letter'].forEach((tabTestId) => {
+        expect(wrapper.find(`[data-test-id=${tabTestId}]`).exists()).toBe(expectedTabTestId === tabTestId)
+      })
+    }
+
+    it('should render tab names', () => {
+      const { tabs } = factory()
+
+      expect(tabs.props('names')).toEqual(['SETTINGS.GENERAL_SETTINGS', 'SETTINGS.PHONEME_SETTINGS', 'SETTINGS.LETTER_SETTINGS'])
+    })
+
+    describe('when initially rendered', () => {
+      it('should initially select the first tab', () => {
+        const { wrapper } = factory()
+
+        expectTabRendered(wrapper, 'general')
+      })
+    })
+
+    describe('when changing the active tab', () => {
+      const cases = [
+        { tabIndex: 0, tabTestId: 'general' },
+        { tabIndex: 1, tabTestId: 'phonemes' },
+        { tabIndex: 2, tabTestId: 'letters' }
+      ]
+      it.each(cases)('should show $tabTestId tab', async ({ tabTestId, tabIndex }) => {
+        const { tabs, wrapper } = factory()
+
+        await tabs.vm.$emit('input', tabIndex)
+
+        expectTabRendered(wrapper, tabTestId)
+      })
+    })
+  })
+
+  describe('general settings', () => {
+    describe('on settings update', () => {
+      it('should emit "update-settings"', () => {
+        const { wrapper } = factory()
+
+        const updateEvent = { key: 'fontSize', value: '140%' }
+        wrapper.find('[data-test-id=general]').vm.$emit('update', updateEvent)
+
+        expect(wrapper.emitted('update-settings')).toEqual([[updateEvent]])
+      })
+    })
+
+    describe('on language update', () => {
+      it('should emit "change-language"', () => {
+        const { wrapper } = factory()
+
+        const changeLanguageEvent = 'FR'
+        wrapper.find('[data-test-id=general]').vm.$emit('change-language', changeLanguageEvent)
+
+        expect(wrapper.emitted('change-language')).toEqual([[changeLanguageEvent]])
+      })
+    })
+  })
+
+  describe('phonemes settings', () => {
+    describe('on phonemes update', () => {
+      it('should emit "update-settings"', async () => {
+        const { wrapper, tabs } = factory()
+
+        await tabs.vm.$emit('input', 1)
+
+        const phonemes: ColoredItem[] = [{ active: false, bold: true, key: '7', value: 'b' }]
+        wrapper.find('[data-test-id=phonemes]').vm.$emit('update-items', phonemes)
+
+        expect(wrapper.emitted('update-settings')).toEqual([[{ key: 'phonemes', value: phonemes }]])
+      })
+
+      describe('on phonemes active update', () => {
+        it('should emit "update-settings"', async () => {
+          const { wrapper, tabs } = factory()
+
+          await tabs.vm.$emit('input', 1)
+
+          const phonemesActive = true
+          wrapper.find('[data-test-id=phonemes]').vm.$emit('update-active', phonemesActive)
+
+          expect(wrapper.emitted('update-settings')).toEqual([[{ key: 'phonemesActive', value: phonemesActive }]])
+        })
+      })
+    })
+  })
+
+  describe('letters settings', () => {
+    describe('on letters update', () => {
+      it('should emit "update-settings"', async () => {
+        const { wrapper, tabs } = factory()
+
+        await tabs.vm.$emit('input', 2)
+
+        const letters: ColoredItem[] = [{ active: false, bold: true, key: '7', value: 'b' }]
+        wrapper.find('[data-test-id=letters]').vm.$emit('update-items', letters)
+
+        expect(wrapper.emitted('update-settings')).toEqual([[{ key: 'letters', value: letters }]])
+      })
+
+      describe('on letters active update', () => {
+        it('should emit "update-settings"', async () => {
+          const { wrapper, tabs } = factory()
+
+          await tabs.vm.$emit('input', 2)
+
+          const lettersActive = true
+          wrapper.find('[data-test-id=letters]').vm.$emit('update-active', lettersActive)
+
+          expect(wrapper.emitted('update-settings')).toEqual([[{ key: 'lettersActive', value: lettersActive }]])
+        })
+      })
+    })
   })
 })
--- apps/base-app/src/features/textProfileForm/ui/TextProfileForm.vue
+++ apps/base-app/src/features/textProfileForm/ui/TextProfileForm.vue
@@ -1,43 +1,62 @@
 <script setup lang="ts">
-import { toRefs } from 'vue'
-import { type Language, type Settings } from '@readapt/settings'
+import { ref, computed } from 'vue'
+import { useI18n } from 'vue-i18n-composable'
+import { type Settings, getLangConfig } from '@readapt/settings'
+import BaseTabs from '@/shared/ui/BaseTabs.vue'
+import SettingsMenuGeneral from '@/views/SettingsMenuGeneral.vue'
+import SettingsMenuTableItems from '@/views/SettingsMenuTableItems.vue'
 
 interface Props {
   settings: Settings
-  activeIndex: number
 }
 const props = defineProps<Props>()
-const { settings, activeIndex } = toRefs(props)
+
+const { t } = useI18n()
+const tabNames = [t('SETTINGS.GENERAL_SETTINGS'), t('SETTINGS.PHONEME_SETTINGS'), t('SETTINGS.LETTER_SETTINGS')]
+
+const activeTabIndex = ref(0)
+
+interface Emits {
+  (event: 'change', value: unknown): void
+}
+const emit = defineEmits<Emits>()
+const emitUpdateSettings = (event: unknown) => emit('update-settings', event)
+
+const languageConfig = computed(() => getLangConfig(props.settings.language))
 </script>
 <template>
   <div>
+    <BaseTabs data-test-id="tabs" :names="tabNames" v-model="activeTabIndex" />
     <SettingsMenuGeneral
-      v-if="activeIndex === 0"
+      v-if="activeTabIndex === 0"
       :settings="settings"
-      @update="updateSettings($event.key, $event.value)"
-      @change-language="setLanguage($event)"
+      data-test-id="general"
+      @update="emitUpdateSettings($event)"
+      @change-language="$emit('change-language', $event)"
     >
     </SettingsMenuGeneral>
     <SettingsMenuTableItems
-      v-if="activeIndex === 1"
+      v-if="activeTabIndex === 1"
       table-label="SETTINGS.PHONEME"
       switch-all-label="SETTINGS.ALL_PHONEMES_SETTINGS"
+      data-test-id="phonemes"
       :all-items-active="settings.phonemesActive"
       :items="settings.phonemes"
       :options="languageConfig.phonemeOptions"
-      @update-items="updateSettings('phonemes', $event)"
-      @update-active="updateSettings('phonemesActive', $event)"
+      @update-items="emitUpdateSettings({ key: 'phonemes', value: $event })"
+      @update-active="emitUpdateSettings({ key: 'phonemesActive', value: $event })"
     >
     </SettingsMenuTableItems>
     <SettingsMenuTableItems
-      v-if="activeIndex === 2"
+      v-if="activeTabIndex === 2"
       table-label="SETTINGS.LETTER"
       switch-all-label="SETTINGS.ALL_LETTERS_SETTINGS"
+      data-test-id="letters"
       :all-items-active="settings.lettersActive"
       :items="settings.letters"
       :options="languageConfig.letterOptions"
-      @update-items="updateSettings('letters', $event)"
-      @update-active="updateSettings('lettersActive', $event)"
+      @update-items="emitUpdateSettings({ key: 'letters', value: $event })"
+      @update-active="emitUpdateSettings({ key: 'lettersActive', value: $event })"
     ></SettingsMenuTableItems>
   </div>
 </template>
