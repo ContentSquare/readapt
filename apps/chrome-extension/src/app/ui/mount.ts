1938a333beb49f6696f28f35974f1a3c03b85627
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-01-27 13:46:30 +0100

    chrome(chrome-app): migrate app mounting

--- apps/chrome-extension/src/app/ui/mount.ts
+++ apps/chrome-extension/src/app/ui/mount.ts
@@ -1,22 +1,21 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './ui/App.vue'
 
 import { withi18n } from './providers/withi18n'
 import { withRouter } from './providers/withRouter'
-import { withSanitize } from './providers/withSanitize'
-import { withVueConfig } from './providers/withVueConfig'
+import VueSanitize from 'vue-3-sanitize'
+// import { withVueConfig } from './providers/withVueConfig'
 
 import './styles/tailwind.scss'
 
 export const mountUI = async () => {
-  withVueConfig()
-  withSanitize()
   const { i18n } = withi18n()
   const { router } = withRouter()
 
-  new Vue({
-    i18n,
-    router,
-    render: (h) => h(App)
-  }).$mount('#app')
+  const app = createApp(App)
+  app.use(i18n)
+  app.use(router)
+  app.use(VueSanitize)
+
+  app.mount('#app')
 }
--- apps/chrome-extension/src/app/ui/providers/withSanitize.ts
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VueSanitize from 'vue-sanitize'
-
-export function withSanitize() {
-  Vue.use(VueSanitize)
-}
