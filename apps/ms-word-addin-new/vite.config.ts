c30215aaf5d323aeb4402e8cb32f9e289d9d1bfa
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-02-03 15:57:40 +0100

    chore(ms-word-addin): configure vite to copy manifest.xml with production url change

--- apps/ms-word-addin-new/vite.config.ts
+++ apps/ms-word-addin-new/vite.config.ts
@@ -4,16 +4,49 @@ import fs from 'node:fs'
 import os from 'node:os'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import pkg from './package.json'
 
+const { env } = process
+
 const HOME_DIRECTORY = os.homedir()
+const IS_PRODUCTION = env.NODE_ENV === 'production'
+
+const copyManifest = viteStaticCopy({
+  targets: [
+    {
+      // TODO: try to refactor this copying to normal building
+      src: '../../packages/visual-engine/dist/readapt-visual-engine.browser.js',
+      dest: 'scripts'
+    },
+    {
+      src: 'manifest*.xml',
+      dest: '.',
+      transform(content) {
+        // @see https://render.com/docs/environment-variables
+        const productionUrl = env.IS_PULL_REQUEST === 'true' || !env.READAPT_MSWORD_URL ? env.RENDER_EXTERNAL_URL : env.READAPT_MSWORD_URL
+        if (!IS_PRODUCTION || !productionUrl) {
+          return content
+        }
+        const DEVELOPMENT_URL = 'https://localhost:5173'
+        return content.toString().replaceAll(DEVELOPMENT_URL, productionUrl)
+      }
+    }
+  ]
+})
+
+const httpsConfigDevMode = {
+  key: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/localhost.key`)),
+  cert: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/localhost.crt`)),
+  ca: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/ca.crt`))
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), copyManifest],
   define: {
     __VERSION__: `"${pkg.version}"`,
-    __MATOMO_URL__: `"${process.env.VUE_APP_MATOMO_URL}"`
+    __MATOMO_URL__: `"${env.VUE_APP_MATOMO_URL}"`
   },
   optimizeDeps: {
     // TODO: try to provide ESM and remove the optimizeDeps
@@ -38,10 +71,6 @@ export default defineConfig({
     }
   },
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/localhost.key`)),
-      cert: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/localhost.crt`)),
-      ca: fs.readFileSync(path.resolve(`${HOME_DIRECTORY}/.office-addin-dev-certs/ca.crt`))
-    }
+    https: IS_PRODUCTION ? undefined : httpsConfigDevMode
   }
 })
