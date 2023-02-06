3f8a44bd8211ef9cbfc4f2d6d76959ea1f31196e
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-02-06 10:07:32 +0100

    refactor(ms-word-addin): remove code duplication

--- apps/ms-word-addin/src/services/stats.ts
+++ apps/ms-word-addin/src/services/stats.ts
@@ -1,12 +1,10 @@
 import { config } from '@/constants/config'
 
 const trackAdaptEvent = (isSelection: boolean) => {
-  const url = `${config.matomoURL}/.php?idsite=2&action_name=adaptSelection&rec=1&ua=unknown&uadata={}`
-  if (isSelection) {
-    fetch(`${url}&action_name=adaptSelection`).catch(console.error)
-  } else {
-    fetch(`${url}&action_name=adaptDocument`).catch(console.error)
-  }
+  let url = `${config.matomoURL}/.php?idsite=2&action_name=adaptSelection&rec=1&ua=unknown&uadata={}`
+  url += isSelection ? '&action_name=adaptSelection' : '&action_name=adaptDocument'
+
+  fetch(url).catch(console.error)
 }
 
 export { trackAdaptEvent }
