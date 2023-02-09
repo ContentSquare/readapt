f6c29813ab17c3e4b3c0c795e9518006e1572994
Dmitri Pavlutin <dpavlutin@gmail.com>
2023-02-09 14:15:04 +0100

    fix(ms-word-addin): use solely local storage in ms word addin

--- apps/ms-word-addin-new/src/app/model/useSetup.ts
+++ apps/ms-word-addin-new/src/app/model/useSetup.ts
@@ -2,6 +2,6 @@ import { textPreferencesStatePersist } from '@/entities/textPreferences'
 import { StorageLocal } from '@/shared/lib/storage'
 
 export function useSetup() {
-  const storage = typeof chrome !== 'undefined' && chrome.extension ? chrome.storage.local : new StorageLocal()
+  const storage = new StorageLocal()
   textPreferencesStatePersist(storage)
 }
